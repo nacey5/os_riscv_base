bcf6c9b325a921f82e52be770657bee65dd2d742
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-05 14:48:28 +0800

    Update common.mk
    路径修改

--- src/code/common.mk
+++ src/code/common.mk
@@ -3,7 +3,6 @@ CFLAGS = -nostdlib -fno-builtin -march=rv32ima -mabi=ilp32 -g -Wall
 
 QEMU = qemu-system-riscv32
 QFLAGS = -nographic -smp 1 -machine virt -bios none
-
 GDB = ${CROSS_COMPILE}gdb
 CC = ${CROSS_COMPILE}gcc
 OBJCOPY = ${CROSS_COMPILE}objcopy
